b22d3c94d3bfa7f37db842e3164edd34d18a9466
jpichon21 <juliencfpichon@gmail.com>
2019-07-31 14:35:00 +0200

    corrige 2 bugs sizing mobile

--- components/connection.php
+++ components/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $pdo = new PDO('mysql:host=localhost;dbname=cms_axelle', 'root');
+    $pdo = new PDO('mysql:host=axellefaazcg6547.mysql.db;dbname=axellefaazcg6547', 'axellefaazcg6547', 'Gutenberg21y');
 } catch (PDOException $e) {
         exit('DB error !');
 }
